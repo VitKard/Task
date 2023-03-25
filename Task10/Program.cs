4d9533b91a03633e3ce41f23f37d40b2d98f3868
VitKard <vit.kard@gmail.com>
2023-03-25 09:42:28 +0300

    done Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! 1111111");
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int number = Prompt("Введите 3-х значное число > ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не 3-х значное число, повторите ввод числа");
+    return;
+}
+Console.WriteLine($"Введённое число '{number}'");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра '{secondRank}'");
