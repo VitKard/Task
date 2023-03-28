0a58618dc1fd350f91f9e92d946a694c457964dc
VitKard <vit.kard@gmail.com>
2023-03-28 20:14:02 +0300

    задача 19

---
+++ Task19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите пятизначное число: ");
+string s=Console.ReadLine()!;
+while (s.Length>5)
+{
+    Console.Write("Вы ошиблись!\nВведите пятизначное число: ");
+    s=Console.ReadLine()!;
+}
+while (s.Length<5)
+{
+    Console.Write("Вы ошиблись!\nВведите пятизначное число: ");
+    s=Console.ReadLine()!;
+}
+if (s[0]==s[4] && s[1]==s[3])
+Console.WriteLine("палиндром");
+else
+Console.WriteLine("не палиндром");
