42c592b038aa0d574d059d18928a4715beff2317
VitKard <vit.kard@gmail.com>
2023-04-01 19:50:14 +0300

    задача 25

---
+++ Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите два числа:  ");
+int A =
+int.Parse(Console.ReadLine()!);
+int D =
+int.Parse(Console.ReadLine()!);
+int result = A;
+for (int i = 1; i < D; i++)
+{
+    result = result * A;
+}
+Console.WriteLine(result);
