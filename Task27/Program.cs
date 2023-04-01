e81ef6b55b015944b41deb4bb00cf266e6469b36
VitKard <vit.kard@gmail.com>
2023-04-01 19:56:53 +0300

    задача 27

---
+++ Task27/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+string s = Console.ReadLine()!;
+int sum = 0;
+for (int i = 0; i < s.Length; i++)
+sum = sum +
+int.Parse(s[i].ToString());
+Console.WriteLine($"сумма цифр числа: {sum}");
