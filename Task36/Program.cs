9bd1e0b03c66002b174e5d910aa7b126d36f7512
VitKard <vit.kard@gmail.com>
2023-04-05 19:50:33 +0300

    Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,21 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-10 , 10);
+}
+int SumNechet(int[] array)
+{   
+    int sum=0;
+    for(int i = 0;i<array.Length;i++)
+    {   if(i%2!=0)
+        {
+            sum+=array[i];
+                            }
+    }
+    return sum;
+}
+Console.Clear();
+int[] array = new int[5];
+InputArray(array);
+System.Console.WriteLine($"array: [{string.Join(" , " , array)}]");
+System.Console.WriteLine($"Сумма элементов на нечетных позициях: {SumNechet(array)}");
