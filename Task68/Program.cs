bcf298b368801f4d15942103f33ddbfe2ddd8c0a
VitKard <vit.kard@gmail.com>
2023-04-24 20:23:02 +0300

    задача 68

---
+++ Task68/Program.cs
@@ -0,0 +1,19 @@
+﻿int Ak(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+        return Ak(m - 1, 1);
+    else
+    {
+        return (Ak(m - 1, Ak(m, n - 1)));
+    }
+}
+Console.Clear();
+System.Console.Write("Введите неотрицательное число: ");
+int m = int.Parse(Console.ReadLine()!);
+System.Console.Write("Введите неотрицательное число: ");
+int n = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine(Ak(m, n));
