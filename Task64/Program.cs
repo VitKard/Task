ce94679efc09cb13acb7f9a479e60cf5876d2f31
VitKard <vit.kard@gmail.com>
2023-04-24 20:12:17 +0300

    задача 64

---
+++ Task64/Program.cs
@@ -0,0 +1,9 @@
+﻿string f(int n)
+{
+    if (n == 1)
+        return "1 ";
+    return $"{n} " + f(n - 1);
+}
+System.Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine(f(n));
