518fedade10dd55b4ff7a9329e925b52edd626a8
VitKard <vit.kard@gmail.com>
2023-04-24 20:17:38 +0300

    задача 66

---
+++ Task66/Program.cs
@@ -0,0 +1,12 @@
+﻿int f(int m, int n)
+{
+    if (m == n)
+        return m;
+    return f(m, n - 1) + n;
+}
+Console.Clear();
+System.Console.Write("Введите начальное число: ");
+int m = int.Parse(Console.ReadLine()!);
+System.Console.Write("Введите конечное число: ");
+int n = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine(f(m, n));
